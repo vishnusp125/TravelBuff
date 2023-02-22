14029a63403ff45fd2f27096e941c7a66ccbe4cc
vishnusp125 <103351267+vishnusp125@users.noreply.github.com>
2023-02-22 20:48:51 +0530

    Update axios.js

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 
-export const axiosUserInstance = axios.create({baseURL:"http://localhost:5000/users"});
-export const axiosAdminInstance = axios.create({baseURL:"http://localhost:5000/admin"});
-export const axiosGuideInstance = axios.create({baseURL:"http://localhost:5000/guide"});
-export const axiosConversationInstance = axios.create({baseURL:"http://localhost:5000/conversations"});
-export const axiosMessageInstance = axios.create({baseURL:"http://localhost:5000/messages"});
+// export const axiosUserInstance = axios.create({baseURL:"http://localhost:5000/users"});
+// export const axiosAdminInstance = axios.create({baseURL:"http://localhost:5000/admin"});
+// export const axiosGuideInstance = axios.create({baseURL:"http://localhost:5000/guide"});
+// export const axiosConversationInstance = axios.create({baseURL:"http://localhost:5000/conversations"});
+// export const axiosMessageInstance = axios.create({baseURL:"http://localhost:5000/messages"});
+
+
+export const axiosUserInstance = axios.create({baseURL:"https://54.238.178.62/users"});
+export const axiosAdminInstance = axios.create({baseURL:"https://54.238.178.62/admin"});
+export const axiosGuideInstance = axios.create({baseURL:"https://54.238.178.62/guide"});
+export const axiosConversationInstance = axios.create({baseURL:"https://54.238.178.62/conversations"});
+export const axiosMessageInstance = axios.create({baseURL:"https://54.238.178.62/messages"});
+
 
 
 
